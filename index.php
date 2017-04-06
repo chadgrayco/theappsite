27949b72e67d2fa4ced0895a2d586dad44192661
kgrayallday <kylemaxgray@gmail.com>
2017-04-06 00:27:33 -0400

    Upload existing
    not sure if this is a legit way to submit changes to the files

--- index.php
+++ index.php
@@ -13,17 +13,20 @@
     
     
 <!--sign in register form-->
-    <form action="posttoform.php" method="post">
-        User: <input type="text" name="uname" id="uname">
+    <form action="signin.php" method="post">
+        user: <input type="text" name="uname" id="uname" placeholder="knock knock who's there?">
         </br>
-        <textarea name="tweeter" id="tweeter" cols="30" rows="10">
-        </textarea>
+        pass: <input type="password" name="password" id="password" placeholder="what's the magic word?">
         </br>
-        <input type="submit">
+        <input type="submit" value="Final Answer!">
     </form>
+    <br>
+    <br>
+
+
 <!--sign up form-->
     <form action="signup.php" method="post">
-        <h1>or... sign up</h1>
+        <h2>or... sign up</h2>
         email:<input type="email" name="email" id="email" placeholder="email"><br>
         username:<input type="text" name="username" id="username" placeholder="username"><br>
         password:<input type="password" name="password" id="password" placeholder="fuck it up"><br>
@@ -38,7 +41,7 @@
 
     
     ?>
-    Remeber getting refund for sponsering sick kids<br>
-    Remeber the skit about needing money back from sponsering kids
+<!--    Remeber getting refund for sponsering sick kids
+    Remeber the skit about needing money back from sponsering kids -->
 </body>
 </html>
--- signin.php
+++ signin.php
@@ -1,5 +1,5 @@
 <?php
-include 'dbh.php';
+/*This file needs a reference to the database (ex. $dbcon was used in signup.php) */
     
 $email = $_POST['email'];
 $username = $_POST['username'];
@@ -11,8 +11,8 @@ $results = mysqli_query($conn, $sql);
 if (!$row = mysqli_fetch_assoc($result)) {
     echo "your username and or password is incorrect";
 } else {
-    echo "your username and or password is correct";
+    echo "your username and password are correct";
 }
 
- header("Location: index.php")
+
 ?>
